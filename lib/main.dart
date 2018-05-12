01d423f8a20284e3d339efb25945e371567e2128
Tensor-Programming <abeltensor@tensor-programming.com>
2018-05-11 20:58:34 -0400

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,147 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData.dark(),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cloud FireStore Example'),
+        centerTitle: true,
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () {
+              Firestore.instance
+                  .runTransaction((Transaction transaction) async {
+                CollectionReference reference =
+                    Firestore.instance.collection('flutter_data');
+
+                await reference
+                    .add({"title": "", "editing": false, "score": 0});
+              });
+            },
+          )
+        ],
+      ),
+      body: StreamBuilder(
+        stream: Firestore.instance.collection('flutter_data').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) return CircularProgressIndicator();
+          return FirestoreListView(documents: snapshot.data.documents);
+        },
+      ),
+    );
+  }
+}
+
+class FirestoreListView extends StatelessWidget {
+  final List<DocumentSnapshot> documents;
+
+  FirestoreListView({this.documents});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: documents.length,
+      itemExtent: 110.0,
+      itemBuilder: (BuildContext context, int index) {
+        String title = documents[index].data['title'].toString();
+        int score = documents[index].data['score'];
+        return ListTile(
+            title: Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5.0),
+                border: Border.all(color: Colors.white),
+              ),
+              padding: EdgeInsets.all(5.0),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: !documents[index].data['editing']
+                        ? Text(title)
+                        : TextFormField(
+                            initialValue: title,
+                            onFieldSubmitted: (String item) {
+                              Firestore.instance
+                                  .runTransaction((transaction) async {
+                                DocumentSnapshot snapshot = await transaction
+                                    .get(documents[index].reference);
+
+                                await transaction.update(
+                                    snapshot.reference, {'title': item});
+
+                                await transaction.update(snapshot.reference,
+                                    {"editing": !snapshot['editing']});
+                              });
+                            },
+                          ),
+                  ),
+                  Text("$score"),
+                  Column(
+                    children: <Widget>[
+                      IconButton(
+                        onPressed: () {
+                          Firestore.instance
+                              .runTransaction((Transaction transaction) async {
+                            DocumentSnapshot snapshot = await transaction
+                                .get(documents[index].reference);
+                            await transaction.update(snapshot.reference,
+                                {'score': snapshot['score'] + 1});
+                          });
+                        },
+                        icon: Icon(Icons.arrow_upward),
+                      ),
+                      IconButton(
+                        onPressed: () {
+                          Firestore.instance
+                              .runTransaction((Transaction transaction) async {
+                            DocumentSnapshot snapshot = await transaction
+                                .get(documents[index].reference);
+                            await transaction.update(snapshot.reference,
+                                {'score': snapshot['score'] - 1});
+                          });
+                        },
+                        icon: Icon(Icons.arrow_downward),
+                      ),
+                    ],
+                  ),
+                  IconButton(
+                    icon: Icon(Icons.delete),
+                    onPressed: () {
+                      Firestore.instance.runTransaction((transaction) async {
+                        DocumentSnapshot snapshot =
+                            await transaction.get(documents[index].reference);
+                        await transaction.delete(snapshot.reference);
+                      });
+                    },
+                  )
+                ],
+              ),
+            ),
+            onTap: () => Firestore.instance
+                    .runTransaction((Transaction transaction) async {
+                  DocumentSnapshot snapshot =
+                      await transaction.get(documents[index].reference);
+
+                  await transaction.update(
+                      snapshot.reference, {"editing": !snapshot["editing"]});
+                }));
+      },
+    );
+  }
+}
